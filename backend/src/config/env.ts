6bacac8e8ce6c1a43f917a7aba043afcc4ce467c
Hinda <profhindo@gmail.com>
2026-04-15 12:23:07 +0300

    fixed small isues

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,6 +7,6 @@ dotenv.config();
 dotenv.config({ path: path.resolve(process.cwd(), "src/.env") });
 
 const PORT = process.env.PORT || 4000;
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY || process.env.OPENWEATHER_API_KEY;
 
 export = { PORT, API_KEY };
